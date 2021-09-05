c8a461f50d6eb2eeaec6c2b4e14d359eb4d33377
Zaid Ahmad <39191309+ZaidAhmad-dev@users.noreply.github.com>
2021-09-05 22:59:19 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./models/article');
+const articleRouter = require('./routes/article');
+const methodOverride = require('method-override');
+const app = express();
+
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true,
+    useUnifiedTopology: true });
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' });
+    
+    res.render('articles/index', {
+        articles: articles
+    });
+});
+
+app.use('/articles', articleRouter);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
